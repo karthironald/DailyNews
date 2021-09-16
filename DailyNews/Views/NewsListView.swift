5bc260c3361e3e2a1cf864fb2904b7354fdf3944
Karthick Selvaraj <karthick@mallow-tech.com>
2021-09-16 23:58:04 +0530

    Have integrated API with pagination

--- DailyNews/Views/NewsListView.swift
+++ DailyNews/Views/NewsListView.swift
@@ -12,15 +12,35 @@ struct NewsListView: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.news, id: \.objectID) { _news in
-                NavigationLink(destination: NewsDetailsView(viewModel: NewsDetailsViewModel(news: _news))) {
-                    NewsRowView(viewModel: NewsRowViewModel(news: _news))
+            List {
+                ForEach(viewModel.news, id: \.objectID) { _news in
+                    NavigationLink(destination: NewsDetailsView(viewModel: NewsDetailsViewModel(news: _news))) {
+                        NewsRowView(viewModel: NewsRowViewModel(news: _news))
+                    }
+                }
+                
+                if !viewModel.isAllHitsFetched && viewModel.news.count > 0 { // To initiate pagination
+                    LoadingView()
+                    .onAppear(perform: {
+                        viewModel.fetchNews(paginating: true, shouldReset: false)
+                    })
                 }
             }
-            .onAppear(perform: {
-                viewModel.fetchNews(paginating: false, shouldReset: false)
-            })
+            .listStyle(PlainListStyle())
             .navigationTitle("News")
+            .navigationBarItems(trailing: loadingView())
+        }
+    }
+ 
+    /**Checks and return the loading view based on loading state*/
+    func loadingView() -> some View {
+        Group {
+            if viewModel.isFetching {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+            } else {
+                EmptyView()
+            }
         }
     }
     
@@ -31,3 +51,4 @@ struct NewsListView_Previews: PreviewProvider {
         NewsListView()
     }
 }
+
