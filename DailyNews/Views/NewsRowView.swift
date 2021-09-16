2920326312a3b629d32e19689dd3448575c0b5a7
Karthick Selvaraj <karthick@mallow-tech.com>
2021-09-16 21:59:52 +0530

    Basic files has been added

---
+++ DailyNews/Views/NewsRowView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsRowView.swift
+//  DailyNews
+//
+//  Created by Karthick Selvaraj on 16/09/21.
+//
+
+import SwiftUI
+
+struct NewsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsRowView()
+    }
+}
