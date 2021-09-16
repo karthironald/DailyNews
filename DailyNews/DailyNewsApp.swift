e5b1d8e9e451ee7f2a3fbe5247f58e73094e34da
Karthick Selvaraj <karthick@mallow-tech.com>
2021-09-16 21:45:02 +0530

    Initial commit

---
+++ DailyNews/DailyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNewsApp.swift
+//  DailyNews
+//
+//  Created by Karthick Selvaraj on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct DailyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
